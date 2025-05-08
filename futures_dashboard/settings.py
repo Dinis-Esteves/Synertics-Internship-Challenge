55048ee3caefd59bf321cf0f67f9ebae98a59244
Dinis Esteves <dinis.esteves@tecnico.ulisboa.pt>
2025-05-08 23:40:12 +0100

    quick fix

--- futures_dashboard/settings.py
+++ futures_dashboard/settings.py
@@ -134,8 +134,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'dashboard/static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379/0")
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
